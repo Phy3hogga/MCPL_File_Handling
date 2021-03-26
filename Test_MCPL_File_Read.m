ba3c684f5191ae8d384c82ddf95f6a7eeb52dbcf
Phy3hogga <alexander.hogg@ntu.ac.uk>
2021-03-26 16:51:28 +0000

    Added more comments and the Data_Operations Submodule for input handling functions

--- Test_MCPL_File_Read.m
+++ Test_MCPL_File_Read.m
@@ -1,19 +1,26 @@
 clear all;
 close all;
-Include_Subdirectories({'Parpool','WinRAR','File_Operations'});
-File_Path = 'C:\Users\alex_\Desktop\MCPL_Output_Diffraction_Test_20210302_123431\MCPL_Output_Diffraction_Test.mcpl.gz';
-%32 bit
+
+%Include directories to external GIT libraries
+Include_Subdirectories({'Parpool','WinRAR','File_Operations', 'Data_Operations'});
+
+%% Test Data
+%32 bit data for testing
 File_Path = 'D:\MCPL_Output_Diffraction_Test_20210304_145727\MCPL_Output_No_Polarisation_Single_Precision.mcpl.gz';
-%64 bit
+%64 bit data for testing
 %File_Path = 'D:\MCPL_Output_Diffraction_Test_20210303_211044\MCPL_Output_Diffraction_Test_1.mcpl.gz';
 
-%% Read parameters
-
+%% Parameters for MCPL processing to MAT file
+%If events are sorted in descending order of weight with the most significant events at the top of the file. (true = sort)
 Read_Parameters.Sort_Events_By_Weight = true;
+%If events with exactly 0 weighting (represent no photons) are to be removed (true = removed)
 Read_Parameters.Remove_Zero_Weights = true;
+%If the temporary files created during processing are deleted (true = delete temp files)
 Read_Parameters.Remove_Temp_Files = true;
+%If the GZ archive has already been uncompressed or not (if problems with WinRAR, can bypass decompression)
 Read_Parameters.Skip_Uncompress = false;
+%Number of cores for the Parpool to use when converting the raw MCPL file (integer)
 Read_Parameters.Parpool_Num_Cores = 6;
 
-%Convert binary MCPL file to MAT file format
+%% Convert MCPL file to MAT file format
 Mat_File_Path = MCPL_To_MAT(File_Path, Read_Parameters);
