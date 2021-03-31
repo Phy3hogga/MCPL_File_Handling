c78b3a64b58bb6d7f604743ab512ec9d719c96cd
Phy3hogga <alexander.hogg@ntu.ac.uk>
2021-03-31 17:45:17 +0100

    Updated input handling and filtering

--- Filter_MPCL_MAT_Data.m
+++ Filter_MPCL_MAT_Data.m
@@ -13,8 +13,12 @@ function Filtered_Mat_File_Path = Filter_MPCL_MAT_Data(Mat_File_Path, Filtered_M
         catch
             error(strcat("Invalid MAT file format: ", Mat_File_Path));
         end
-        %Validate relevant variables exist within the MAT file
-        if(all(ismember({'Header','X','Y','Z','Dx', 'Dy', 'Dz','Weight'}, Mat_File_Variables)))
+        %% Validate relevant variables exist within the MAT file
+        if(all(ismember({'X','Y','Z','Dx', 'Dy', 'Dz','Weight'}, Mat_File_Variables)))
+        else
+            error("Filter_MCPL_MAT_Data: Missing key variables within original MAT file");
+        end
+        if(all(ismember({'Header'}, Mat_File_Variables)))
         else
             error("Filter_MCPL_MAT_Data: Missing key variables within original MAT file");
         end
@@ -47,15 +51,117 @@ function Filtered_Mat_File_Path = Filter_MPCL_MAT_Data(Mat_File_Path, Filtered_M
         else
             Exists.Time = false;
         end
+        %Remove header from the variable list (it's only ever a 1x1 struct)
+        Mat_File_Variables(strcmp(Mat_File_Variables, 'Header')) = [];
+        %Validate the length of each field in the structure matches
+        Size_1 = zeros(length(Mat_File_Variables),1);
+        Size_2 = zeros(length(Mat_File_Variables),1);
+        for Current_Field = 1:length(Mat_File_Variables)
+            [Size_1(Current_Field), Size_2(Current_Field)] = size(Mat_File_Reference, Mat_File_Variables{Current_Field});
+        end
+        if (~(std(Size_1(:)) == 0 && std(Size_2(:)) == 0))
+            error("Filter_MCPL_MAT_Data: Mismatch in variable sizes.");
+        end
         %If applicable; create the directory path to save the 
-        Filtered_Mat_File_Path = fileparts(Filtered_Mat_File_Path);
-        if(~isfolder(Filtered_Mat_File_Path))
-            Directory_Created = Attempt_Directory_Creation(Filtered_Mat_File_Path);
+        Filtered_Mat_Directory_Path = fileparts(Filtered_Mat_File_Path);
+        if(~isfolder(Filtered_Mat_Directory_Path))
+            Directory_Created = Attempt_Directory_Creation(Filtered_Mat_Directory_Path);
             if(~Directory_Created)
-                error(strcat("Could not create directory tree to : ", Filtered_Mat_File_Path));
+                error(strcat("Could not create directory path to : ", Filtered_Mat_Directory_Path));
             end
         end
-        %Filtered_Mat_File_Reference = matfile();
+        %Check Filters is a structure
+        if(~isstruct(Filters))
+            error("Filter_MCPL_MAT_Data: Filters expected to be a structure.");
+        end
+        %Verify the fields in the structure against valid filter types
+        Allowed_Filters = {'X', 'Y', 'Z', 'Energy', 'Weight', 'Angle'};
+        Active_Filters = fieldnames(Filters);
+        Remove_Filters = zeros(size(Active_Filters), 'logical');
+        for Current_Active_Filter = 1:length(Active_Filters)
+            Filter_Allowed = any(strcmpi(Allowed_Filters, Active_Filters{Current_Active_Filter}));
+            if(~Filter_Allowed)
+                Remove_Filters(Current_Active_Filter) = 1;
+                disp(strcat("Filter_MCPL_MAT_Data: Ignoring Invalid Filter : ", Active_Filters{Current_Active_Filter}));
+            end
+        end
+        %Delete any invalid filter requests
+        Active_Filters(Remove_Filters) = [];
+        
+        %% Generic sense-checks for structure inputs
+        %Default values
+        for Current_Active_Filter = 1:length(Active_Filters)
+            Filters.(Active_Filters{Current_Active_Filter}).Min_Active = false;
+            Filters.(Active_Filters{Current_Active_Filter}).Max_Active = false;
+        end
+        for Current_Active_Filter = 1:length(Active_Filters)
+            if(isstruct(Filters.(Active_Filters{Current_Active_Filter})))
+                %Find Min and Max fields
+                Filter_Min_Max = fieldnames(Filters.(Active_Filters{Current_Active_Filter}));
+                Filters.(Active_Filters{Current_Active_Filter}).Min_Active = false;
+                %Verify Min field exists
+                if(any(strcmpi(Filter_Min_Max, 'Min')))
+                    if(isnumeric(Filters.(Active_Filters{Current_Active_Filter}).Min))
+                        Filters.(Active_Filters{Current_Active_Filter}).Min_Active = true;
+                    else
+                        warning(strcat("Filter_MCPL_MAT_Data : Expected Numeric Input for Minimum ", Active_Filters{Current_Active_Filter}, " : Ignoring Filter"));
+                    end
+                end
+                Filters.(Active_Filters{Current_Active_Filter}).Max_Active = false;
+                %Verify Max field exists
+                if(any(strcmpi(Filter_Min_Max, 'Max')))
+                    if(isnumeric(Filters.(Active_Filters{Current_Active_Filter}).Max))
+                        Filters.(Active_Filters{Current_Active_Filter}).Max_Active = true;
+                    else
+                        warning(strcat("Filter_MCPL_MAT_Data : Expected Numeric Input for Maximum ", Active_Filters{Current_Active_Filter}, " : Ignoring Filter"));
+                    end
+                end
+                %If both min and max are active; check they are appropriately ordered (min < max)
+                if(Filters.(Active_Filters{Current_Active_Filter}).Max_Active && Filters.(Active_Filters{Current_Active_Filter}).Min_Active)
+                    if(Filters.(Active_Filters{Current_Active_Filter}).Max < Filters.(Active_Filters{Current_Active_Filter}).Min)
+                        Temp = Filters.(Active_Filters{Current_Active_Filter}).Max;
+                        Filters.(Active_Filters{Current_Active_Filter}).Max = Filters.(Active_Filters{Current_Active_Filter}).Min;
+                        Filters.(Active_Filters{Current_Active_Filter}).Min = Temp;
+                        warning(strcat("Filter_MCPL_MAT_Data : Swapped Min and Max bounds for ", Active_Filters{Current_Active_Filter}));
+                    end
+                end
+                %Minimum value exceptions for angle end energy
+                if(strcmpi(Active_Filters{Current_Active_Filter}, 'Energy') || strcmpi(Active_Filters{Current_Active_Filter}, 'Angle'))
+                    if(Filters.(Active_Filters{Current_Active_Filter}).Min_Active)
+                        if(Filters.(Active_Filters{Current_Active_Filter}).Min < 0)
+                            Filters.(Active_Filters{Current_Active_Filter}).Min = 0;
+                            warning(strcat("Filter_MCPL_MAT_Data : Minimum ", Active_Filters{Current_Active_Filter}, " trunicated to 0"));
+                        end
+                    end
+                end
+                %maximum value exception for angle
+                if(strcmpi(Active_Filters{Current_Active_Filter}, 'Angle'))
+                    if(Filters.(Active_Filters{Current_Active_Filter}).Max_Active)
+                        if(Filters.(Active_Filters{Current_Active_Filter}).Max > 90)
+                            Filters.(Active_Filters{Current_Active_Filter}).Max = 90;
+                            warning(strcat("Filter_MCPL_MAT_Data : Minimum ", Active_Filters{Current_Active_Filter}, " trunicated to 90"));
+                        end
+                    end
+                end
+                % load data and set Allowed_Index_List to false where the current event doesn't meet the required conditions.
+                if(Filters.(Active_Filters{Current_Active_Filter}).Max_Active || Filters.(Active_Filters{Current_Active_Filter}).Min_Active)
+                    Data = Mat_File_Reference.({Current_Active_Filter});
+                    
+                end
+            else
+                disp(strcat("Filter_MCPL_MAT_Data: Ignoring Invalid Filter Structure : ", Active_Filters{Current_Active_Filter}));
+            end
+        end
+        clear Filter_Min_Max Current_Active_Filter;
+        
+        
+        %% Create a list of all indexed present within the original MAT file
+        Allowed_Index_List = ones(Size_1(1), 1, 'logical');
+        clear Size_1 Size_2 Filtered_Mat_Directory_Path Remove_Filters;
+        
+        %Create filtered MAT file
+        Filtered_Mat_File_Reference = matfile(Filtered_Mat_File_Path);
+        Filtered_Mat_File_Reference.Properties.Writable = true;
     else
         error("Filter_MCPL_Mat_Data: MAT file not found");
     end
